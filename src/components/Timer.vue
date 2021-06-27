76d9a16ced70cead79c666741ed44d30adcc84ea
Coaxial <thecoaxial@gmail.com>
2021-06-27 13:09:32 -0400

    re aligned the colored circle to better match the counter, added color classes for warning and times up,  need to fix issue of colored ring not animating

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,4 +1,3 @@
-<!--Base Timer-->
 <template>
   <div class="base-timer">
     <svg
@@ -11,15 +10,16 @@
           class="base-timer__path-elapsed"
           cx="50"
           cy="50"
-          r="46.5"
-        />
+          r="45"
+        ></circle>
         <path
+          :stroke-dasharray="circleDasharray"
           class="base-timer__path-remaining"
           d="
             M 50, 50
             m -45, 0
             a 45,45 0 1,0 90,0
-            a 45,45 0 1,0 90,0
+            a 45,45 0 1,0 -90,0
           "></path>
       </g>
     </svg>
@@ -30,9 +30,28 @@
 </template>
 
 <script>
+const FULL_DASH_ARRAY = 283;
 export default {
+   data() {
+    return {
+      timeLimit: 20,
+      timePassed: 0,
+      timerInterval: null,
+    }
+  },
+  methods: {
+    startTimer() {
+      this.timerInterval = setInterval (() => (this.timePassed +=1), 1000)
+    }
+  },
+  mounted() {
+    this.startTimer();
+  },
   
   computed: {
+    timeLeft() {
+      return this.timeLimit - this.timePassed
+    },
     formattedTimeLeft() {
       const timeLeft = this.timeLeft
 
@@ -49,15 +68,25 @@ export default {
 
       // the output in MM:SS format
       return `${minutes}:${seconds}`
+    },
+
+    circleDasharray() {
+        return `${(this.timeFraction * FULL_DASH_ARRAY).toFixed(0)}283`;
+    },
+    
+    timeFraction() {
+      const rawTimeFraction = this.timeLeft/ this.timeLimit
+
+      return rawTimeFraction - (1 / this.timeLimit) * ( 1- rawTimeFraction)
     }
   },
 
-  props: {
-    timeLeft: {
-      type: Number,
-      required: true
-    }
-  }  
+  // props: {
+  //   timeLeft: {
+  //     type: Number,
+  //     required: true
+  //   }
+  // }  
 }
 </script>
 
@@ -92,6 +121,34 @@ export default {
 
     font-size: 48px;
   }
+
+  &__path-remaining {
+    stroke-width: 7px;
+    stroke-linecap: round;
+    transform: rotate(90deg);
+    transform-origin: center;
+    transition: 1s linear all;
+    fill-rule: nonzero;
+    stroke: currentColor;
+
+    &.green {
+      color: rgb(65, 184, 131);
+    }
+
+    &.orange {
+      color: orange;
+    }
+
+    &.red {
+      color: red;
+    }
+  }
+
+  &__svg {
+    transform: scaleX(-1);
+  }
 }
 </style>>
+
+
 
