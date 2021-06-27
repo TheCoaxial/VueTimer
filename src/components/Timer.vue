1767161c1e81d3c1ba22c384504a3be3387a2c90
Coaxial <thecoaxial@gmail.com>
2021-06-27 13:18:38 -0400

    color now updates properly and ring animation plays

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,93 +1,115 @@
 <template>
-  <div class="base-timer">
-    <svg
-      class="base-timer__svg"
-      viewBox="0 0 100 100"
-      xmlns="http://www.w3.org/2000/svg"
-    >
+    <div class="base-timer">
+    <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
       <g class="base-timer__circle">
-        <circle
-          class="base-timer__path-elapsed"
-          cx="50"
-          cy="50"
-          r="45"
-        ></circle>
+        <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
         <path
           :stroke-dasharray="circleDasharray"
           class="base-timer__path-remaining"
+          :class="remainingPathColor"
           d="
             M 50, 50
             m -45, 0
             a 45,45 0 1,0 90,0
             a 45,45 0 1,0 -90,0
-          "></path>
+          "
+        ></path>
       </g>
     </svg>
-    <span class="base-timer__label">
-      {{ formattedTimeLeft }}
-    </span>
+    <span class="base-timer__label">{{ formattedTimeLeft }}</span>
   </div>
 </template>
 
 <script>
 const FULL_DASH_ARRAY = 283;
+const WARNING_THRESHOLD = 10;
+const ALERT_THRESHOLD = 5;
+
+const COLOR_CODES = {
+  info: {
+    color: "green"
+  },
+  warning: {
+    color: "orange",
+    threshold: WARNING_THRESHOLD
+  },
+  alert: {
+    color: "red",
+    threshold: ALERT_THRESHOLD
+  }
+};
+
+const TIME_LIMIT = 300;
+
 export default {
-   data() {
+  data() {
     return {
-      timeLimit: 20,
       timePassed: 0,
-      timerInterval: null,
+      timerInterval: null
+    };
+  },
+
+  computed: {
+    circleDasharray() {
+      return `${(this.timeFraction * FULL_DASH_ARRAY).toFixed(0)} 283`;
+    },
+
+    formattedTimeLeft() {
+      const timeLeft = this.timeLeft;
+      const minutes = Math.floor(timeLeft / 60);
+      let seconds = timeLeft % 60;
+
+      if (seconds < 10) {
+        seconds = `0${seconds}`;
+      }
+
+      return `${minutes}:${seconds}`;
+    },
+
+    timeLeft() {
+      return TIME_LIMIT - this.timePassed;
+    },
+
+    timeFraction() {
+      const rawTimeFraction = this.timeLeft / TIME_LIMIT;
+      return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+    },
+
+    remainingPathColor() {
+      const { alert, warning, info } = COLOR_CODES;
+
+      if (this.timeLeft <= alert.threshold) {
+        return alert.color;
+      } else if (this.timeLeft <= warning.threshold) {
+        return warning.color;
+      } else {
+        return info.color;
+      }
     }
   },
-  methods: {
-    startTimer() {
-      this.timerInterval = setInterval (() => (this.timePassed +=1), 1000)
+
+  watch: {
+    timeLeft(newValue) {
+      if (newValue === 0) {
+        this.onTimesUp();
+      }
     }
   },
+
   mounted() {
     this.startTimer();
   },
-  
-  computed: {
-    timeLeft() {
-      return this.timeLimit - this.timePassed
-    },
-    formattedTimeLeft() {
-      const timeLeft = this.timeLeft
 
-      //The largest round integer less than or equal to the result of time divided by 60
-      const minutes = Math.floor(timeLeft / 60)
-
-      // Seconds calculations
-      let seconds = timeLeft % 60
-
-      // If the value of seconds is less than 10 then display seconds left with a leading 0
-      if (seconds < 10) {
-        seconds = `0${seconds}`
-      }
-
-      // the output in MM:SS format
-      return `${minutes}:${seconds}`
+  methods: {
+    onTimesUp() {
+      clearInterval(this.timerInterval);
     },
 
-    circleDasharray() {
-        return `${(this.timeFraction * FULL_DASH_ARRAY).toFixed(0)}283`;
-    },
-    
-    timeFraction() {
-      const rawTimeFraction = this.timeLeft/ this.timeLimit
-
-      return rawTimeFraction - (1 / this.timeLimit) * ( 1- rawTimeFraction)
+    startTimer() {
+      this.timerInterval = setInterval(() => (this.timePassed += 1), 1000);
     }
-  },
-
-  // props: {
-  //   timeLeft: {
-  //     type: Number,
-  //     required: true
-  //   }
-  // }  
-}
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
