2c9e9e0e382dbe3e372cc81f8df7fc01089b5cde
Coaxial <thecoaxial@gmail.com>
2021-06-27 11:58:15 -0400

    replaced HelloWorld.vue with basic shape of timer. Also replaced references to deleted file in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <template>
+
+  
+
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
@@ -6,7 +9,7 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/Timer.vue'
 
 export default {
   name: 'App',
